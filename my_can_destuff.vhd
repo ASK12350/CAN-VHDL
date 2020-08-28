19c758cf3f0fccb25ecb4b0e5c5c82d92efd9d0b
ASK12350 <68654267+ASK12350@users.noreply.github.com>
2020-08-28 14:36:29 +0530

    added tx to entity

--- my_can_destuff.vhd
+++ my_can_destuff.vhd
@@ -12,12 +12,20 @@ ENTITY my_can_bsp IS
 		  sampled_bit  : IN std_logic;
 		  sampled_bit_q: IN std_logic;
 		  id_ok        : IN std_logic;
+		  txfifo_empty : IN std_logic;
+		  rxfifo_full  : IN std_logic;
+		  data_in      : IN std_logic_vector(7 DOWNTO 0);
+		    tx         : OUT std_logic;
+			 transmitter: OUT std_logic;
+			 transmitting: OUT std_logic;
 		    ide_out    : OUT std_logic;
 			 acf_en     : OUT std_logic;
 			 go_error   : OUT std_logic;
 			 msg_end    : OUT std_logic;
 			 id         : OUT std_logic_vector(28 DOWNTO 0) ;
 		    reset_mode : OUT std_logic;
+			 fifo_wr_en : OUT std_logic;
+			 fifo_rd_en : OUT std_logic;
 		       data_out    : OUT std_logic_vector(7 DOWNTO 0));
 END my_can_bsp;
 
@@ -50,7 +58,7 @@ SIGNAL go_rx_ack_de : std_logic;
 SIGNAL go_rx_eof    : std_logic;
 SIGNAL go_rx_idle   : std_logic;
 SIGNAL go_rx_inter  : std_logic;
-SIGNAL go_rx_presof : std_logic;
+--SIGNAL go_rx_presof : std_logic;
 SIGNAL go_rx_sof    : std_logic;
 
 --go_errror
@@ -61,7 +69,7 @@ SIGNAL go_form_error  : std_logic;
 --rx_state
 SIGNAl rx_idle      : std_logic;
 SIGNAL rx_inter     : std_logic;
-SIGNAL rx_presof    : std_logic;
+--SIGNAL rx_presof    : std_logic;
 SIGNAL rx_sof       : std_logic;
 SIGNAL rx_id1       : std_logic;
 SIGNAL rx_id2       : std_logic;
@@ -95,7 +103,8 @@ SIGNAL ack_de       : std_logic;
 
 --writing into temporary storage
 TYPE msg_array IS ARRAY(0 TO 12) OF std_logic_vector(7 DOWNTO 0);
-SIGNAL msg_buffer   : msg_array;
+SIGNAL msg_rx_buffer   : msg_array;
+SIGNAL msg_tx_buffer   : msg_array;
 SIGNAL wr           : std_logic;
 SIGNAL i            : integer range  0 to 12;
 --destuff
@@ -115,9 +124,14 @@ SIGNAL crc_temp      : std_logic_vector(14 DOWNTO 0);
 --fifo
 SIGNAL wr_fifo      : std_logic;
 SIGNAL cs_fifo      : std_logic;
+SIGNAL fifo_wr_en_temp : std_logic;
+SIGNAL fifo_rd_en_temp : std_logic;
 SIGNAL wr_cnt       : integer range 0 TO 12 ;
 SIGNAL msg_len      : integer range 0 TO 12 ;
 
+SIGNAL transmitter_temp : std_logic;
+SIGNAL transmitting_temp: std_logic;
+SIGNAL msg_tx_buffer_empty : std_logic;
 SIGNAL remote_frame    : std_logic;
 
 FUNCTION conv(b: boolean) return std_logic IS
@@ -134,10 +148,12 @@ BEGIN
 uucrc : my_can_crc PORT MAP    (    clk=>clk,
                                     clr=>crc_clr,
 								            enable=>crc_enable,
-							               data=>sampled_bit_q,
+							               data=>sampled_bit,
 							            	crc_out=>crc_temp   );
 
 
+fifo_wr_en_temp <=wr_fifo and cs_fifo and not(rxfifo_full);
+fifo_rd_en_temp <=not(wr_fifo) and cs_fifo and not(txfifo_empty);
 ide_out <=ide;
 msg_len <=12 WHEN (not(remote_frame) and conv(dlc(3)='1')) ='1' ELSE
           to_integer(unsigned(dlc))+4 WHEN remote_frame='0' ELSE
@@ -148,14 +164,14 @@ crc_enable <= crc_en and sample_point and not(destuff);
 crc_clr    <= rst or rx_idle;
  
 --go_rx_signals 
-go_rx_presof   <= hard_sync and (rx_idle or rx_inter);
-go_rx_sof      <= (sample_point and rx_presof);
+go_rx_sof   <= hard_sync and (rx_idle or rx_inter);
+--go_rx_sof      <= (sample_point and rx_presof);
 go_rx_id1      <= (sample_point and rx_sof);
 go_rx_rtr1     <= (sample_point and not(destuff)) and conv(cnt=10) and rx_id1;
 go_rx_ide      <= (sample_point and not(destuff)) and rx_rtr1;
-go_rx_id2      <= (sample_point and not(destuff)) and sampled_bit_q and rx_ide;
+go_rx_id2      <= (sample_point and not(destuff)) and sampled_bit and rx_ide;
 go_rx_rtr2     <= (sample_point and not(destuff)) and conv(cnt=17) and rx_id2;
-go_rx_rb0      <= (sample_point and not(destuff)) and ((not(sampled_bit_q) and rx_ide) or (ide and rx_rb1));
+go_rx_rb0      <= (sample_point and not(destuff)) and ((not(sampled_bit) and rx_ide) or (ide and rx_rb1));
 go_rx_rb1      <= (sample_point and not(destuff)) and rx_rtr2 and ide; 
 go_rx_dlc      <= (sample_point and not(destuff)) and rx_rb0;
 go_rx_data     <= (sample_point and not(destuff)) and conv(cnt=3) and rx_dlc and not(remote_frame);
@@ -170,31 +186,44 @@ go_rx_idle     <= (sample_point and rx_inter) and conv(cnt=2);
 --go_error signals
 go_crc_error   <= go_rx_eof and not(conv(crc=crc_temp));
 go_stuff_error <= sample_point and conv(destuff_cnt=4) and (sampled_bit xnor sampled_bit_q) and destuff_en ;
-go_form_error <=  sample_point and not(destuff) and (rx_ack_de or rx_crc_de or rx_eof) and not(sampled_bit_q);
+go_form_error <=  sample_point and not(destuff) and (rx_ack_de or rx_eof) and not(sampled_bit);
 go_error <=go_form_error or go_stuff_error or go_crc_error;
 
 remote_frame    <= ((not(ide) and rtr1) or (ide and rtr2));
 
-destuff_en      <= not(rx_idle or rx_inter or rx_eof or rx_ack or rx_crc_de or rx_ack_de or rx_presof);
+destuff_en      <= not(rx_idle or rx_inter or rx_eof or rx_ack or rx_crc_de or rx_ack_de or rx_sof);
 
 dlc_int         <= 63 WHEN dlc(3)='1' ELSE
                    to_integer(8*unsigned(dlc))-1;
+						 
+						 
+--fifo_en
+PROCESS(clk,rst)
+BEGIN
+  IF(rst='1') THEN
+   fifo_wr_en <='0';
+	fifo_rd_en <='0';
+  ELSIF(rising_edge(clk)) THEN
+   fifo_wr_en <=fifo_wr_en_temp;
+   fifo_rd_en <=fifo_rd_en_temp;	
+  END IF;
+END PROCESS;  
 
 --data into a temporary storage
 PROCESS(clk,rst)
 BEGIN
   IF(rst='1') THEN
-   msg_buffer<=(others=>(others=>'0'));
+   msg_rx_buffer<=(others=>(others=>'0'));
   ELSIF(rising_edge(clk)) THEN
-   msg_buffer(0)    <= id1(10 DOWNTO 3);
-   msg_buffer(1)    <= id1(2 DOWNTO 0) & ide & dlc;
-   msg_buffer(2)    <= remote_frame & "00000" & id2(17 DOWNTO 16);
-   msg_buffer(3)    <= id2(15 DOWNTO 8);
-   msg_buffer(4)    <= id2(7 DOWNTO 0);
+   msg_rx_buffer(0)    <= id1(10 DOWNTO 3);
+   msg_rx_buffer(1)    <= id1(2 DOWNTO 0) & ide & dlc;
+   msg_rx_buffer(2)    <= remote_frame & "00000" & id2(17 DOWNTO 16);
+   msg_rx_buffer(3)    <= id2(15 DOWNTO 8);
+   msg_rx_buffer(4)    <= id2(7 DOWNTO 0);
     IF(wr='1') THEN
-	  msg_buffer(i)<=data_temp;
+	  msg_rx_buffer(i)<=data_temp;
 	 ELSIF(rx_idle='1') THEN
-	  msg_buffer<=(others=>(others=>'0'));
+	  msg_rx_buffer<=(others=>(others=>'0'));
 	 END IF;
   END IF;
 END PROCESS;
@@ -248,18 +277,18 @@ END PROCESS;
 
 --rx state changing
 
-PROCESS(clk,rst)
-BEGIN
-  IF(rst='1') THEN
-   rx_presof <='0';
-  ELSIF(rising_edge(clk)) THEN
-    IF(go_rx_presof='1') THEN
- 	  rx_presof <='1';
-	 ELSIF(go_rx_sof='1') THEN
-	  rx_presof <='0';
-	 END IF;
-  END IF;
-END PROCESS;
+--PROCESS(clk,rst)
+--BEGIN
+  --IF(rst='1') THEN
+  -- rx_presof <='0';
+  --ELSIF(rising_edge(clk)) THEN
+  --  IF(go_rx_presof='1') THEN
+  --  rx_presof <='1';
+  -- ELSIF(go_rx_sof='1') THEN
+  --  rx_presof <='0';
+  -- END IF;
+  --END IF;
+--END PROCESS;
 
 --sof
 PROCESS(clk,rst)
@@ -493,7 +522,7 @@ BEGIN
   ELSIF(rising_edge(clk)) THEN
     IF(go_rx_idle='1') THEN
 	  rx_idle <='1';
-	 ELSIF(go_rx_presof='1') THEN
+	 ELSIF(go_rx_sof='1') THEN
 	  rx_idle<='0';
 	 END IF;
   END IF;
@@ -523,7 +552,7 @@ BEGIN
    id1 <= (others=>'1');
   ELSIF(rising_edge(clk)) THEN
     IF((rx_id1 and sample_point and not(destuff))='1') THEN
-     id1 <=	id1(9 DOWNTO 0) & sampled_bit_q;
+     id1 <=	id1(9 DOWNTO 0) & sampled_bit;
     ELSIF(rx_idle='1') THEN
 	  id1 <= (others=>'1');
 	 END IF;
@@ -537,7 +566,7 @@ BEGIN
    rtr1 <= '1';
   ELSIF(rising_edge(clk)) THEN
     IF((rx_rtr1 and sample_point and not(destuff))='1') THEN
-     rtr1 <= sampled_bit_q;
+     rtr1 <= sampled_bit;
     ELSIF(rx_idle='1') THEN
 	  rtr1 <='1';
 	 END IF;
@@ -551,7 +580,7 @@ BEGIN
    ide <= '1';
   ELSIF(rising_edge(clk)) THEN
     IF((rx_ide and sample_point and not(destuff))='1') THEN
-     ide <=	sampled_bit_q;
+     ide <=	sampled_bit;
     ELSIF(rx_idle='1') THEN
 	  ide <='1';
 	 END IF;
@@ -565,7 +594,7 @@ BEGIN
    id2 <= (others=>'0');
   ELSIF(rising_edge(clk)) THEN
     IF((rx_id2 and sample_point and not(destuff))='1') THEN
-     id2 <=	id2(16 DOWNTO 0) & sampled_bit_q;
+     id2 <=	id2(16 DOWNTO 0) & sampled_bit;
     ELSIF(rx_idle='1') THEN
 	  id2 <= (others=> '0');
 	 END IF;
@@ -579,7 +608,7 @@ BEGIN
    rtr2 <= '1';
   ELSIF(rising_edge(clk)) THEN
     IF((rx_rtr2 and sample_point and not(destuff))='1') THEN
-     rtr2 <= sampled_bit_q;
+     rtr2 <= sampled_bit;
     ELSIF(rx_idle='1') THEN
 	  rtr2 <='1';
 	 END IF;
@@ -593,7 +622,7 @@ BEGIN
    rb1 <= '1';
   ELSIF(rising_edge(clk)) THEN
     IF((rx_rb1 and sample_point and not(destuff))='1') THEN
-     rb1 <=	sampled_bit_q;
+     rb1 <=	sampled_bit;
     ELSIF(rx_idle='1') THEN
 	  rb1 <= '1';
 	 END IF;
@@ -607,7 +636,7 @@ BEGIN
    rb0 <= '1';
   ELSIF(rising_edge(clk)) THEN
     IF((rx_rb0 and sample_point and not(destuff))='1') THEN
-     rb0 <=	sampled_bit_q;
+     rb0 <=	sampled_bit;
     ELSIF(rx_idle='1') THEN
 	  rb0 <='1';
 	 END IF;
@@ -621,7 +650,7 @@ BEGIN
    dlc <= (others=>'1');
   ELSIF(rising_edge(clk)) THEN
     IF((rx_dlc and sample_point and not(destuff))='1') THEN
-     dlc <=	dlc(2 DOWNTO 0) & sampled_bit_q;
+     dlc <=	dlc(2 DOWNTO 0) & sampled_bit;
     ELSIF(rx_idle='1') THEN
 	  dlc <=(others=>'1');
 	 END IF;
@@ -635,7 +664,7 @@ BEGIN
    data_temp <= (others=>'1');
   ELSIF(rising_edge(clk)) THEN
     IF((rx_data and sample_point and not(destuff))='1') THEN
-     data_temp <= data_temp(6 DOWNTO 0) & sampled_bit_q;
+     data_temp <= data_temp(6 DOWNTO 0) & sampled_bit;
     ELSIF(rx_idle='1') THEN 
 	  data_temp <= (others=>'1');
 	 END IF;
@@ -649,7 +678,7 @@ BEGIN
    crc <= (others=>'0');
   ELSIF(rising_edge(clk)) THEN
     IF((rx_crc and sample_point and not(destuff))='1') THEN
-     crc <= crc(13 DOWNTO 0) & sampled_bit_q;
+     crc <= crc(13 DOWNTO 0) & sampled_bit;
     ELSIF(rx_idle='1') THEN
 	  crc <=(others=>'0');
 	 END IF;
@@ -663,7 +692,7 @@ BEGIN
    crc_de <= '1';
   ELSIF(rising_edge(clk)) THEN
     IF((rx_crc_de and sample_point and not(destuff))='1') THEN
-     crc_de <= sampled_bit_q;
+     crc_de <= sampled_bit;
     ELSIF(rx_idle='1') THEN
 	  crc_de <='1';
 	 END IF;
@@ -677,7 +706,7 @@ BEGIN
    ack <= '1';
   ELSIF(rising_edge(clk)) THEN
     IF((rx_ack and sample_point )='1') THEN
-     ack <= sampled_bit_q;
+     ack <= sampled_bit;
     ELSIF(rx_idle='1') THEN
 	  ack <='1';
 	 END IF;
@@ -691,7 +720,7 @@ BEGIN
    ack_de <= '1';
   ELSIF(rising_edge(clk)) THEN
     IF((rx_ack_de and sample_point)='1') THEN
-     ack_de <= sampled_bit_q;
+     ack_de <= sampled_bit;
     ELSIF(rx_idle='1') THEN 
 	  ack_de <='1';
 	 END IF;
@@ -718,23 +747,24 @@ BEGIN
   END IF;
 END PROCESS;
 
+destuff <= sample_point and destuff_en and conv(destuff_cnt=4);
 --destuff when 5 consecutive identical bits
-PROCESS(clk,rst) 
-BEGIN
-  IF(rst='1') THEN
-   destuff <='0';
-  ELSIF(rising_edge(clk)) THEN
-    IF(sample_point='1') THEN
-	   IF(destuff_en='0') THEN
-	    destuff <='0';
-		ELSIF(destuff_cnt=4) THEN
-		 destuff <='1';
-		ELSE
-		 destuff <='0';
-		END IF;
-	 END IF;
-  END IF;
-END PROCESS;
+--PROCESS(clk,rst) 
+--BEGIN
+--  IF(rst='1') THEN
+--   destuff <='0';
+--  ELSIF(rising_edge(clk)) THEN
+--    IF(sample_point='1') THEN
+--	   IF(destuff_en='0') THEN
+--	    destuff <='0';
+--		ELSIF(destuff_cnt=4) THEN
+--		 destuff <='1';
+--		ELSE
+--		 destuff <='0';
+--		END IF;
+--	 END IF;
+-- END IF;
+--END PROCESS;
 
 --crc_enable
 PROCESS(clk,rst)
@@ -842,7 +872,7 @@ BEGIN
    data_out <= (others=>'0') ;
   ELSIF(rising_edge(clk)) THEN 
     IF((cs_fifo and wr_fifo)='1') THEN
-	  data_out <=msg_buffer(wr_cnt);
+	  data_out <=msg_rx_buffer(wr_cnt);
 	 ELSE
 	  data_out <=(others=>'0');
 	 END IF;
